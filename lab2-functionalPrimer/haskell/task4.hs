94b911e9929eb69ad0a71b66986147375b8b533d
Simran <240364746@aston.ac.uk>
2026-02-10 12:40:39 +0000

    task4 completed

--- lab2-functionalPrimer/haskell/task4.hs
+++ lab2-functionalPrimer/haskell/task4.hs
@@ -4,8 +4,12 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    then ask (prompt ++ "!") --append ! to prompt each time empty string entered
+    else if (line == "quit") then putStrLn("quitting...")
+    -- condition added for quitting
+    else do 
+      putStrLn ("you said: " ++ reverse line)
+      ask prompt 
 
 main :: IO ()
 main =
