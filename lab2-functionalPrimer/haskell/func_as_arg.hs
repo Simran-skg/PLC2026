a854d8c3d7b6f33edf0574b68552a30dbffee803
Simran <240364746@aston.ac.uk>
2026-02-10 12:53:15 +0000

    task6

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -19,14 +19,3 @@ main = do
 
 
 #A more general version of func_as_arg.py that works for a set of Ints between a and b
- 
-def listFunc(a, b): 
-  return [i for i in range(a, b+1)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
- 
-def applicatorFunc(inpFunc, s, a, b):
-  if s=='s': 
-    return sum(inpFunc(a, b)) 
-  else: 
-    return sum(inpFunc(a, b))/(b-a+1)
- 
-print(applicatorFunc(listFunc, 'a', 1, 10))
